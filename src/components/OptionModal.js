b8560475765a4f91860dbd276bd21f7c9ab12bf7
Phonekham <phonekham.dev@gmail.com>
2019-06-05 09:36:27 +0700

    061 Setting up React-Modal

---
+++ src/components/OptionModal.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Modal from "react-modal";
+
+const OptionModal = props => (
+  <Modal isOpen={!!props.selectedOption} contentLabel="Selected option">
+    <h3>Select option</h3>
+    {props.selectedOption && <p>{props.selectedOption}</p>}
+    <button onClick={props.handleClearSelectedOption}>Ok</button>
+  </Modal>
+);
+
+export default OptionModal;
