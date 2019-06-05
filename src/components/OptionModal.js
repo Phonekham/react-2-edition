201b9268dcb6d2990bd0ba135e24e6d4060e63ed
Phonekham <phonekham.dev@gmail.com>
2019-06-05 11:56:13 +0700

    Finish styling

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -2,10 +2,18 @@ import React from "react";
 import Modal from "react-modal";
 
 const OptionModal = props => (
-  <Modal isOpen={!!props.selectedOption} contentLabel="Selected option">
-    <h3>Select option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.handleClearSelectedOption}>Ok</button>
+  <Modal
+    className="modal"
+    isOpen={!!props.selectedOption}
+    contentLabel="Selected option"
+  >
+    <h3 className="modal__title">Select option</h3>
+    {props.selectedOption && (
+      <p className="modal__body">{props.selectedOption}</p>
+    )}
+    <button className="button" onClick={props.handleClearSelectedOption}>
+      Ok
+    </button>
   </Modal>
 );
 
